0a683c11dbe3491a037aa50cd8c053de56bea5ea
StephenDelaCruz <stephendelacruzz@gmail.com>
2024-08-23 19:26:43 +1200

    UI update

--- app/src/main/java/com/skysphere/skysphere/ui/gallery/SavedLocationFragment.kt
+++ app/src/main/java/com/skysphere/skysphere/ui/gallery/SavedLocationFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.skysphere.skysphere.databinding.FragmentGalleryBinding
+import com.skysphere.skysphere.databinding.FragmentSavedlocationBinding
 
-class GalleryFragment : Fragment() {
+class SavedLocationFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentSavedlocationBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        val savedLocationViewModel =
+            ViewModelProvider(this).get(SavedLocationViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentSavedlocationBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textSavedLocation
+        savedLocationViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
