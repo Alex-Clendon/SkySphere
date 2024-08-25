922e3cb7878c2c88981a790218bbfeed127b736c
StephenDelaCruz <stephendelacruzz@gmail.com>
2024-08-25 20:02:23 +1200

    Adding weather icons, and general update.

--- app/src/main/java/com/skysphere/skysphere/ui/location/LocationFragment.kt
+++ app/src/main/java/com/skysphere/skysphere/ui/location/LocationFragment.kt
@@ -1,4 +1,4 @@
-package com.skysphere.skysphere.ui.savedLocation
+package com.skysphere.skysphere.ui.location
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.skysphere.skysphere.databinding.FragmentSavedlocationBinding
+import com.skysphere.skysphere.databinding.FragmentLocationBinding
 
-class SavedLocationFragment : Fragment() {
+class LocationFragment : Fragment() {
 
-    private var _binding: FragmentSavedlocationBinding? = null
+    private var _binding: FragmentLocationBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class SavedLocationFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val savedLocationViewModel =
-            ViewModelProvider(this).get(SavedLocationViewModel::class.java)
+        val locationViewModel =
+            ViewModelProvider(this).get(LocationViewModel::class.java)
 
-        _binding = FragmentSavedlocationBinding.inflate(inflater, container, false)
+        _binding = FragmentLocationBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSavedLocation
-        savedLocationViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textLocation
+        locationViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/skysphere/skysphere/ui/location/LocationViewModel.kt
+++ app/src/main/java/com/skysphere/skysphere/ui/location/LocationViewModel.kt
@@ -1,10 +1,10 @@
-package com.skysphere.skysphere.ui.savedLocation
+package com.skysphere.skysphere.ui.location
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SavedLocationViewModel : ViewModel() {
+class LocationViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is the Saved Locations Fragment"
