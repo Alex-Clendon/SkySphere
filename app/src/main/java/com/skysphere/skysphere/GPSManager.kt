31956d1613ec60aa191104b16dc1d2e65a54dbd8
Alex-Clendon <gmc1891@autuni.ac.nz>
2024-09-01 19:16:05 +1200

    Implementing location dependencies and skeleton GPS manager class

---
+++ app/src/main/java/com/skysphere/skysphere/GPSManager.kt
@@ -0,0 +1,11 @@
+package com.skysphere.skysphere
+
+import com.google.android.gms.location.FusedLocationProviderClient
+
+class GPSManager {
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private var latitude: Double = 0.0
+    private var longitude: Double = 0.0
+
+
+}
