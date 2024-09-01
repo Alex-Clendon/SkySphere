f3ad815972aebc746bf126f3036c05a2126ca368
Alex-Clendon <gmc1891@autuni.ac.nz>
2024-09-01 21:01:21 +1200

    Building the GPSManager class, uses android location dependencies (fused location provider) and will be the class used for activities and fragments needing fine user location.

--- app/src/main/java/com/skysphere/skysphere/GPSManager.kt
+++ app/src/main/java/com/skysphere/skysphere/GPSManager.kt
@@ -1,11 +1,39 @@
 package com.skysphere.skysphere
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Location
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 
-class GPSManager {
-    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-    private var latitude: Double = 0.0
-    private var longitude: Double = 0.0
+class GPSManager(private val context: Context){
+    private var fusedLocationProviderClient: FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
+    private var currentLocation: Location? = null // Store current location
 
+    private val locationRequest: LocationRequest = LocationRequest.create().apply{ // Sets parameters for location updates
+        priority = LocationRequest.PRIORITY_HIGH_ACCURACY // Setting the priority to high accuracy
+    }
+
+    private val locationCallback = object : LocationCallback() { // Defines what happens when a location update is received
+        override fun onLocationResult(locationResult: LocationResult){
+            currentLocation = locationResult.lastLocation // Update the current location
+        }
+    }
+
+    @SuppressLint("MissingPermission") // Permission is determined by the caller
+    fun startLocationUpdates() { // Start receiving location updates based on locationRequest parameters
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null)
+    }
+
+    fun stopLocationUpdates() { // Stop receiving location updates and remove the location callback
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+    }
+
+    fun getCurrentLocation(): Location? { // Return the most recent location from currentLocation.
+        return currentLocation
+    }
 
 }
