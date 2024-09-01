d1be00f20865390ce4b0587ad464d1036a23cfeb
kjd-dev <kjd2221@autuni.ac.nz>
2024-09-01 20:29:33 +1200

    Implemented google map to the locations page, and refactored some references.

---
+++ app/src/main/java/com/skysphere/skysphere/ui/location/LocationsFragment.kt
@@ -0,0 +1,34 @@
+package com.skysphere.skysphere.ui.location
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.skysphere.skysphere.R
+
+class LocationsFragment : Fragment(), OnMapReadyCallback {
+
+    private var mGoogleMap:GoogleMap? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_locations, container, false)
+
+        // Initialize the map fragment
+        val mapFragment = childFragmentManager.findFragmentById(R.id.googleMap) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        return view
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mGoogleMap = googleMap
+
+    }
+}
