15a7a2fd9015f08b9813d1be2398f213198bbd81
Tuitauofiti <qws7323@autuni.ac.nz>
2024-09-11 20:37:56 +1200

    Added comments to the codes I had created in the SettingsFragment.kt file

--- app/src/main/java/com/skysphere/skysphere/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/skysphere/skysphere/ui/settings/SettingsFragment.kt
@@ -13,7 +13,7 @@ import com.skysphere.skysphere.R
 
 class SettingsFragment : Fragment()
 {
-    // Initializing
+    // Initializing variables to store user temperature metric unit preferences
     private lateinit var sharedPreferences: SharedPreferences
     private val temperatureUnitKey = "temperature_unit"
     private lateinit var temperatureUnitTextView: TextView
@@ -24,41 +24,52 @@ class SettingsFragment : Fragment()
     ): View? {
         val view = inflater.inflate(R.layout.fragment_settings, container, false)
 
+        // Initializing a SharedPreferences object called sharedPreferences so that the user can access the
+        // shared preferences, allowing you to read and write preferences (such as user settings or application state)
+        // in a persistent storage
         sharedPreferences = requireActivity().getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
 
+        // Assigning the buttons to their corresponding variables declared above
         val cButton: Button = view.findViewById(R.id.Celsius)
         val fButton: Button = view.findViewById(R.id.Fahrenheit)
         val kButton: Button = view.findViewById(R.id.Kelvin)
+
+        // Assigning the views to their corresponding variables declared above
         temperatureUnitTextView = view.findViewById(R.id.temp_details)
 
+        // Setting up the buttons in the settings fragment xml file with their corresponding temperature metric unit
         cButton.setOnClickListener {
             saveTemperatureUnit("Celsius")
         }
-
         fButton.setOnClickListener{
             saveTemperatureUnit("Fahrenheit")
         }
-
         kButton.setOnClickListener {
             saveTemperatureUnit("Kelvin")
         }
 
+        // Initializing the TextView Temperature Details with the current temperature unit
         updateTemperatureUnitTextView()
 
         return view
     }
 
+    // Saving the preference for the temperature metric unit of the user
     private fun saveTemperatureUnit(unit: String){
         val editor = sharedPreferences.edit()
         editor.putString(temperatureUnitKey, unit)
         editor.apply()
 
+        // Updating the TextView Temperature Details with the chosen temperature unit
         updateTemperatureUnitTextView()
     }
 
+    // Retrieving the stored preference for temperature metric unit of the user
     private fun updateTemperatureUnitTextView() {
         val sharedPreferences = requireActivity().getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
         val unit = sharedPreferences.getString("temperature_unit", "Celsius") ?: "Celsius"
+
+        // This sets the TextView for the Temperature Details to what it equates and displays it onto the settings page
         temperatureUnitTextView.text = "The temperature unit is currently set to $unit"
     }
 }
