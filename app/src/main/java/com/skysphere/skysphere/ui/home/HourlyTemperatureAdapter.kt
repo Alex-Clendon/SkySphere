77e47517fe3acb3e4b94d2de708092341d48909b
kjd-dev <kjd2221@autuni.ac.nz>
2024-09-16 04:36:24 +1200

    Created function in HourlyTemperatureAdapter.kt that generates a list of times starting from any hour, not just midnight.

--- app/src/main/java/com/skysphere/skysphere/ui/home/HourlyTemperatureAdapter.kt
+++ app/src/main/java/com/skysphere/skysphere/ui/home/HourlyTemperatureAdapter.kt
@@ -13,9 +13,7 @@ class HourlyTemperatureAdapter(
 ) : RecyclerView.Adapter<HourlyTemperatureAdapter.HourlyViewHolder>() {
 
     // Generating times from 00:00 to 23:00
-    private val times: List<String> = List(24) { index ->
-        String.format("%02d:00", index)
-    }
+    private val times: List<String> = generateTimes(0)
 
     // Called when recyclerview needs a new viewholder to display an item
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HourlyViewHolder {
@@ -42,4 +40,12 @@ class HourlyTemperatureAdapter(
         val tvHour: TextView = view.findViewById(R.id.tvHour)
         val tvHourlyTemperature: TextView = view.findViewById(R.id.tvHourlyTemperature)
     }
+
+    fun generateTimes(startHour: Int): List<String> {
+        return List(24) { index ->
+            val hour = (startHour + index) % 24
+            String.format("%02d:00", hour)
+        }
+    }
+
 }
