275a1123508b73f2846349f54e6ac237faca5809
kjd-dev <kjd2221@autuni.ac.nz>
2024-09-20 18:01:32 +1200

    Added Log In page to Nav Drawer

--- app/src/main/java/com/skysphere/skysphere/MainActivity.kt
+++ app/src/main/java/com/skysphere/skysphere/MainActivity.kt
@@ -36,7 +36,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_locations, R.id.nav_settings
+                R.id.nav_home, R.id.nav_locations, R.id.nav_settings, R.id.nav_login
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
