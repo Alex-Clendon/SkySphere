a729a6eb8bd5b2afacf7e8dcf06b3360710ee2ee
kjd-dev <kjd2221@autuni.ac.nz>
2024-09-20 17:30:15 +1200

    Started development of SignupFragment logic

--- app/src/main/java/com/skysphere/skysphere/SignupFragment.kt
+++ app/src/main/java/com/skysphere/skysphere/SignupFragment.kt
@@ -1,26 +1,51 @@
 package com.skysphere.skysphere
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import android.widget.Toast
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class SignupFragment : Fragment() {
 
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var firebaseDatabase: FirebaseDatabase
+    private lateinit var databaseReference: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
 
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        databaseReference = firebaseDatabase.reference.child("users")
+    }
+
+    private fun signupUser(username: String, password: String) {
+
+        databaseReference.orderByChild("username").equalTo(username).addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                if(!dataSnapshot.exists()) {
+                    val email = databaseReference.push().key
+                    val userData = UserData(email, username, password)
+                    databaseReference.child(email!!).setValue(userData)
+                    Toast.makeText(requireContext(), "Successfully Registered!", Toast.LENGTH_SHORT).show()
+                    parentFragmentManager.popBackStack()
+                }
+                else {
+                    Toast.makeText(requireContext(), "User already exists", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Toast.makeText(requireContext(), "Database Error: ${databaseError.message}", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 
     override fun onCreateView(
--- app/src/main/java/com/skysphere/skysphere/UserData.kt
+++ app/src/main/java/com/skysphere/skysphere/UserData.kt
@@ -2,7 +2,7 @@ package com.skysphere.skysphere
 
 data class UserData(
 
-    val username: String? = null,
     val email: String? = null,
+    val username: String? = null,
     val password: String? = null
 )
