b4fd048d0c611f92687b6ab608219dc34aa276f0
StephenDelaCruz <stephendelacruzz@gmail.com>
2024-10-06 23:16:51 +1300

    UI update, and initialised my UI components in the AddFriendsFragment.

--- app/src/main/java/com/skysphere/skysphere/ui/friends/AddFriendsFragment.kt
+++ app/src/main/java/com/skysphere/skysphere/ui/friends/AddFriendsFragment.kt
@@ -4,11 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageButton
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.skysphere.skysphere.R
 
 class AddFriendsFragment : Fragment() {
+
+    private lateinit var searchBar: EditText
+    private lateinit var searchButton: ImageButton
+    private lateinit var searchResults: RecyclerView
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
