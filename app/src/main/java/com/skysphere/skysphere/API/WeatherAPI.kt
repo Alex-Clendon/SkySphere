70794b22b4df110ce1f86ff65d738e19cf7a45ff
neilluna8 <126937750+neilluna8@users.noreply.github.com>
2024-10-13 21:04:16 +1300

    accidentally deleted weatherapi file

---
+++ app/src/main/java/com/skysphere/skysphere/API/WeatherAPI.kt
@@ -0,0 +1,17 @@
+package com.skysphere.skysphere.API
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherAPI {
+    @GET("v1/forecast")
+    fun getWeatherData(
+        @Query("latitude") latitude: Double,
+        @Query("longitude") longitude: Double,
+        @Query("current") current: String,
+        @Query("daily") daily: String,
+        @Query("timezone") timezone: String,
+        @Query("hourly") hourly: String
+    ): Call<WeatherData>
+}
