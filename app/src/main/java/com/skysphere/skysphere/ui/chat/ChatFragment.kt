0c0b8dcbd3c9d954ca4167f25640ddcb972f957a
StephenDelaCruz <stephendelacruzz@gmail.com>
2024-10-16 14:25:12 +1300

    Progress on Chat feature. Messages are sent to the database, however, it isn't displaying the messages yet. Still need to fix.

--- app/src/main/java/com/skysphere/skysphere/ui/chat/ChatFragment.kt
+++ app/src/main/java/com/skysphere/skysphere/ui/chat/ChatFragment.kt
@@ -1,5 +1,7 @@
 package com.skysphere.skysphere.ui.chat
 
+import android.annotation.SuppressLint
+import android.icu.util.Calendar
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,7 +12,10 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -18,11 +23,18 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.skysphere.skysphere.R
 import com.skysphere.skysphere.UserData
+import java.text.SimpleDateFormat
 
 class ChatFragment : Fragment() {
 
+    private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var firebaseDatabase: FirebaseDatabase
     private lateinit var usersRef: DatabaseReference
+    private lateinit var msgsRef: DatabaseReference
+
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var msgAdapter: MessagesAdapter
+    private lateinit var messagesList: MutableList<Messages>
 
     private lateinit var receiverUsername : TextView
     private lateinit var messages : RecyclerView
@@ -30,14 +42,22 @@ class ChatFragment : Fragment() {
     private lateinit var msgInput : EditText
 
     private lateinit var msgReceiverId : String
+    private var currentUserId : String? = null
+
+    private lateinit var saveCurrentDate: String
+    private lateinit var saveCurrentTime: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        firebaseAuth = FirebaseAuth.getInstance()
         firebaseDatabase = FirebaseDatabase.getInstance()
         usersRef = firebaseDatabase.reference.child("users")
+        msgsRef = firebaseDatabase.reference
 
         msgReceiverId = arguments?.getString("userId") ?: ""
+        currentUserId = firebaseAuth.currentUser?.uid
+        messagesList = mutableListOf<Messages>()
     }
 
     override fun onCreateView(
@@ -54,9 +74,25 @@ class ChatFragment : Fragment() {
         sendMsg = view.findViewById(R.id.sendButton)
         msgInput = view.findViewById(R.id.sendMsg)
 
+        initialiseChat()
+
         loadUsername(msgReceiverId)
 
+        sendMsg.setOnClickListener {
+            sendMessage()
+        }
+
+        fetchMessages()
+
         return view
+    }
+
+    private fun initialiseChat() {
+        msgAdapter = MessagesAdapter(messagesList)
+        linearLayoutManager = LinearLayoutManager(context)
+        messages.setHasFixedSize(true)
+        messages.layoutManager = linearLayoutManager
+        messages.adapter = msgAdapter
     }
 
     private fun loadUsername(userId: String) {
@@ -72,6 +108,86 @@ class ChatFragment : Fragment() {
                 Toast.makeText(context, "Failed to load username", Toast.LENGTH_SHORT).show()
             }
         })
+    }
+
+    private fun sendMessage() {
+        val message = msgInput.text.toString()
+
+        if(message.isEmpty()){
+            Toast.makeText(context, "Please enter a message", Toast.LENGTH_SHORT).show()
+        }else{
+            val message_sender_ref = "messages/ $currentUserId/$msgReceiverId"
+            val message_receiver_ref = "messages/ $msgReceiverId/$currentUserId"
+
+            val user_msg_key = msgsRef.child("messages").child(currentUserId.toString()).child(msgReceiverId).push()
+
+            val msg_push_id = user_msg_key.key
+
+            val getDate : Calendar
+            getDate = Calendar.getInstance()
+            val currentDate : SimpleDateFormat
+            currentDate = SimpleDateFormat("dd-MMMM-yyyy")
+            saveCurrentDate = currentDate.format(getDate.time)
+
+            val getTime : Calendar
+            getTime = Calendar.getInstance()
+            val currentTime : SimpleDateFormat
+            currentTime = SimpleDateFormat("HH:mm aa")
+            saveCurrentTime = currentTime.format(getTime.time)
+
+            val msgMap = HashMap<String, Any>()
+            msgMap["message"] = message
+            msgMap["time"] = saveCurrentTime
+            msgMap["date"] = saveCurrentDate
+            msgMap["type"] = "text"
+            msgMap["from"] = currentUserId.toString()
+
+            val msgMapDetails = HashMap<String, Any>()
+            msgMapDetails.put(message_sender_ref + "/" + msg_push_id, msgMap)
+            msgMapDetails.put(message_receiver_ref + "/" + msg_push_id, msgMap)
+
+            msgsRef.updateChildren(msgMapDetails).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Toast.makeText(context, "Message sent successfully", Toast.LENGTH_SHORT).show()
+                    msgInput.text.clear()
+                } else {
+                    val errorMsg = it.exception?.message
+                    Toast.makeText(context, "Error: "+errorMsg, Toast.LENGTH_SHORT).show()
+                    msgInput.text.clear()
+                }
+            }
+        }
+    }
+
+    private fun fetchMessages() {
+        msgsRef.child("messages").child(currentUserId.toString()).child(msgReceiverId)
+            .addChildEventListener(object : ChildEventListener {
+                override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                    if(snapshot.exists()){
+                        val message : Messages
+                        message = snapshot.getValue(Messages::class.java)!!
+                        messagesList.add(message)
+                        msgAdapter.notifyDataSetChanged()
+                    }
+                }
+
+                override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+
+                }
+
+                override fun onChildRemoved(snapshot: DataSnapshot) {
+
+                }
+
+                override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+            })
     }
 
 }
---
+++ app/src/main/java/com/skysphere/skysphere/ui/chat/MessagesAdapter.kt
@@ -0,0 +1,63 @@
+package com.skysphere.skysphere.ui.chat
+
+import android.graphics.Color
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.skysphere.skysphere.R
+
+class MessagesAdapter(messagesList: MutableList<Messages>) : RecyclerView.Adapter<MessagesAdapter.MessageViewHolder>() {
+
+    private val messagesList = messagesList
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    class MessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val senderMsg = itemView.findViewById<TextView>(R.id.senderMsg)
+        val receiverMsg = itemView.findViewById<TextView>(R.id.receiverMsg)
+        val msgProfileImage = itemView.findViewById<TextView>(R.id.msgProfileImage)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.msg_format, parent, false)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        return MessageViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
+        val currentUserId = firebaseAuth.currentUser?.uid
+        val message : Messages = messagesList.get(position)
+        val fromUserID = message.from
+        val fromMessageType = message.type
+
+        if(fromMessageType.equals("text")){
+            holder.receiverMsg.visibility = View.INVISIBLE
+            holder.msgProfileImage.visibility = View.INVISIBLE
+
+            if (fromUserID.equals(currentUserId)) {
+                holder.senderMsg.setBackgroundResource(R.drawable.msg_sender_text_background)
+                holder.senderMsg.setTextColor(Color.WHITE)
+                holder.senderMsg.gravity = Gravity.LEFT
+                holder.senderMsg.text = message.message
+            }else{
+                holder.senderMsg.visibility = View.INVISIBLE
+                holder.receiverMsg.visibility = View.VISIBLE
+                holder.msgProfileImage.visibility = View.VISIBLE
+
+                holder.receiverMsg.setBackgroundResource(R.drawable.msg_receiver_text_background)
+                holder.receiverMsg.setTextColor(Color.WHITE)
+                holder.receiverMsg.gravity = Gravity.LEFT
+                holder.receiverMsg.text = message.message
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return messagesList.size
+    }
+}
