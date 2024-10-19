a7b9f9805f3229ca0f957104a6c49d72c0314981
kjd-dev <kjd2221@autuni.ac.nz>
2024-10-20 09:27:01 +1300

    Added new icons to daily forecast

--- app/src/main/java/com/skysphere/skysphere/ui/adapters/DailyWeatherAdapter.kt
+++ app/src/main/java/com/skysphere/skysphere/ui/adapters/DailyWeatherAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.airbnb.lottie.LottieAnimationView
 import com.skysphere.skysphere.API.WeatherType
 import com.skysphere.skysphere.R
 import com.skysphere.skysphere.data.weather.WeatherDaily
@@ -23,7 +24,8 @@ class DailyWeatherAdapter
     // ViewHolder to hold the views for each row
     inner class DailyWeatherViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val dayTextView: TextView = itemView.findViewById(R.id.textViewDay)
-        val weatherImageView: ImageView = itemView.findViewById(R.id.imageViewWeather)
+        val weatherImageView: LottieAnimationView = itemView.findViewById(R.id.imageViewWeather)
+        val nightImageView: LottieAnimationView = itemView.findViewById(R.id.imageView2)
         val tempMaxTextView: TextView = itemView.findViewById(R.id.textViewTemp)
         val precipitationTextView: TextView = itemView.findViewById(R.id.textViewPrecipitation)
 
@@ -43,12 +45,10 @@ class DailyWeatherAdapter
     override fun onBindViewHolder(holder: DailyWeatherViewHolder, position: Int) {
 
         val day = dailyWeather?.day?.get(position)
-        val weatherCode = dailyWeather?.weatherCode?.get(position)
+        val weatherType = dailyWeather?.weatherType?.get(position)
         val tempMax = dailyWeather?.roundedTemperatureMax?.get(position)
         val tempMin = dailyWeather?.roundedTemperatureMin?.get(position)
         val precipitation = dailyWeather?.precipitationProbability?.get(position)
-
-        val weatherImageRes = WeatherType.fromWMO(weatherCode ?: 0).iconRes
 
         if (position == 0) {
             holder.dayTextView.text = "Today"
@@ -56,7 +56,12 @@ class DailyWeatherAdapter
             holder.dayTextView.text = day ?: ""
         }
         holder.precipitationTextView.text = precipitation.toString() + "%"
-        holder.weatherImageView.setImageResource(weatherImageRes)
+        if (weatherType != null) {
+            holder.weatherImageView.setAnimation(weatherType.lottieAnimRes)
+            holder.weatherImageView.playAnimation()
+        }
+        holder.nightImageView.setAnimation((R.raw.partly_cloudy_night))
+        holder.nightImageView.playAnimation()
         holder.tempMaxTextView.text = tempMax?.toString() + "° " + tempMin.toString() + "°"
     }
 
